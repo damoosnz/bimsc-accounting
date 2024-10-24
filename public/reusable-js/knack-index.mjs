35c9558bfda50fb518f3bf66c5af88066557e8b1
damoosnz <damien.legrand@bimsolutionscentre.com>
2024-10-25 00:16:31 +1300

    update post function

--- public/reusable-js/knack-index.mjs
+++ public/reusable-js/knack-index.mjs
@@ -1,6 +1,7 @@
 //Manage your Knack code from here, never directly in the Knack builder
 
 const netlifyBaseUrl = 'https://bimsc-admin.netlify.app';
+const localBaseUrl = 'http://localhost:3000/'
 
 //You can import external packages from URL via skypack CDN
 //These will work in the browser due to magic done by skypack
@@ -8,3 +9,4 @@ const netlifyBaseUrl = 'https://bimsc-admin.netlify.app';
 import KnackAPI from 'https://cdn.skypack.dev/knack-api-helper@2.2.4'
 import axios from 'https://cdn.skypack.dev/axios';
 
+import './bimsc-financials/renders/add-transaction-render.js'
