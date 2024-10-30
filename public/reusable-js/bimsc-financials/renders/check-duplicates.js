ee19d708964e7b075d5231f85592a12bbeb40508
damoosnz <damien.legrand@bimsolutionscentre.com>
2024-10-31 12:43:47 +1300

    test with rollup

--- public/reusable-js/bimsc-financials/renders/check-duplicates.js
+++ public/reusable-js/bimsc-financials/renders/check-duplicates.js
@@ -1,12 +1,13 @@
-import { knackInterface } from 'https://cdn.jsdelivr.net/gh/damoosnz/bimsc-knack-api@main/index.js';
+import { knackInterface } from 'https://cdn.jsdelivr.net/gh/damoosnz/bimsc-knack-interface@main/dist/bimsc-knack-interface.bundle.js'
+// import { bwKnackApi } from '../../to-bundle-for-nodejs/to-bundle.js';
 
 // scene_26 view_45
 
 $(document).on('knack-view-render.view_45', async function (event, view, data) {
 
     knackInterface.msg.popup.add(view.key, 'loading in progress', 'spinner')//
-    const payload = knackApi.payloads.getMany('scene_26', 'view_45')
-    const records = await knackApi.calls.getMany(payload)
+    const payload = bwKnackApi.payloads.getMany('scene_26', 'view_45')
+    const records = await bwKnackApi.calls.getMany(payload)
     console.log(records)
 
 });
---
+++ public/reusable-js/to-bundle-for-nodejs/to-bundle.js
@@ -0,0 +1,5 @@
+import { knackApi } from "bimsc-knack-api";
+
+export const bwKnackApi = { knackApi }
+
+
