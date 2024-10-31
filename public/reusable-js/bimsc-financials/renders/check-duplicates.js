96019422e6711fce2e7fd299737f332eb23988ec
damoosnz <damien.legrand@bimsolutionscentre.com>
2024-10-31 19:26:08 +1300

    replace rollup with esbuild

--- public/reusable-js/bimsc-financials/renders/check-duplicates.js
+++ public/reusable-js/bimsc-financials/renders/check-duplicates.js
@@ -1,13 +1,13 @@
-import { knackInterface } from 'https://cdn.jsdelivr.net/gh/damoosnz/bimsc-knack-interface@main/dist/bimsc-knack-interface.bundle.js'
-// import { bwKnackApi } from '../../to-bundle-for-nodejs/to-bundle.js';
+import { knackInterface } from 'https://cdn.jsdelivr.net/gh/damoosnz/bimsc-knack-interface@main/dist/bimsc-knack-interface.bundle-v1.js'
+import { knackApi } from 'https://cdn.jsdelivr.net/gh/damoosnz/bimsc-knack-api@main/dist/bundle-v1.js';
 
 // scene_26 view_45
 
 $(document).on('knack-view-render.view_45', async function (event, view, data) {
 
     knackInterface.msg.popup.add(view.key, 'loading in progress', 'spinner')//
-    const payload = bwKnackApi.payloads.getMany('scene_26', 'view_45')
-    const records = await bwKnackApi.calls.getMany(payload)
+    const payload = knackApi.payloads.getMany('scene_26', 'view_45')
+    const records = await knackApi.calls.getMany(payload)
     console.log(records)
 
 });
